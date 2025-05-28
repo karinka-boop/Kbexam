07a67786a26ebad8d972c65473bf9718b28e9848
karinka-boop <kbaimukhamedova06@gmail.com>
2025-05-28 13:09:58 +0300

    Update admin.py

--- exam_site/exams/admin.py
+++ exam_site/exams/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
-from .models import Kbexam  # Замените на вашу модель
+from .models import kbexam
 
-class KbexamAdmin(admin.ModelAdmin):
+class kbexamAdmin(admin.ModelAdmin):
     list_display = ('title', 'created_at', 'exam_date', 'is_public')
     list_filter = ('is_public', 'created_at')
     search_fields = ('title', 'users__email')
     date_hierarchy = 'exam_date'
-    filter_horizontal = ('users',)
+    filter_horizontal = ('users',)  # Для удобного выбора студентов
 
-admin.site.register(Kbexam, KbexamAdmin)  # Замените на вашу модель
+admin.site.register(kbexam, kbexamAdmin)
